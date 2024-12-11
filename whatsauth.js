6d18fa1403515c55f738ce12cc229406da63d5da
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-12-11 17:59:30 +0700

    Update keyword URL in whatsauth.js for improved functionality

--- whatsauth.js
+++ whatsauth.js
@@ -4,7 +4,7 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.3.3/confi
 
 //definisikan url wss dan keyword menggunakan base64
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTk3MTAwNDA/dGV4dD13aDR0NWF1dGgw";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTE3NjAwNTA/dGV4dD13aDR0NWF1dGgw";
 wauthparam.redirect="#";
 
 //delete cookies session and call whatsauth qrController
